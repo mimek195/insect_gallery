5c65b12ee485e621c10fa0e8681f92d20971c39b
mimek <jelonekiv@gmail.com>
2025-01-05 16:50:58 +0100

    testing git commit

---
+++ classes.py
@@ -0,0 +1,12 @@
+
+
+class Entry:
+    print("test")
+
+
+class TaxonomicRank:
+    def __init__(self, rank, rank_name, super_taxon=None):
+        taxon_rank = rank
+        taxon_rank_name = rank_name
+        taxon_super_taxon = super_taxon
+
