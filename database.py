86433f7b5daa2ce4585d9603bed9ddab812c572c
mimek <jelonekiv@gmail.com>
2025-09-01 14:42:29 +0200

    formating, input supertaxon id

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import sqlite3
 import os
 
-
 def create_taxonomic_database():
     taxonomy = sqlite3.connect('taxonomy.db')
     cursor = taxonomy.cursor()
@@ -16,25 +15,25 @@ def create_taxonomic_database():
     ''')
 
     ranks = [
-        (1, "Phylum", 0),
-        (2, "Class", 1),
-        (3, "Subclass", 2),
-        (4, "Infraclass", 3),
-        (5, "Order", 4),
-        (6, "Suborder", 5),
-        (7, "Superfamily", 6),
-        (8, "Family", 7),
-        (9, "Subfamily", 8),
-        (10, "Tribe", 9),
-        (11, "Genus", 10),
-        (12, "Species", 11)
+        (1, "Species", 0),
+        (2, "Genus", 1),
+        (3, "Tribe", 2),
+        (4, "Subfamily", 3),
+        (5, "Family", 4),
+        (6, "Superfamily", 5),
+        (7, "Suborder", 6),
+        (8, "Order", 7),
+        (9, "Infraclass", 8),
+        (10, "Subclass", 9),
+        (11, "Class", 10),
+        (12, "Phylum", 11)
     ]
     cursor.executemany('''
         INSERT INTO ranks (id, name, super_rank_id) VALUES (?, ?, ?)
         ''', ranks)
 
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS taxonomic_rank (
+    CREATE TABLE IF NOT EXISTS taxons (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         rank_id INTEGER NOT NULL,
         name_latin TEXT NOT NULL,
@@ -52,7 +51,7 @@ def create_taxonomic_database():
         photo_location TEXT NOT NULL,
         photo_description TEXT,
         description TEXT,
-        FOREIGN KEY(species_id) REFERENCES taxonomic_rank(id) ON DELETE CASCADE
+        FOREIGN KEY(species_id) REFERENCES taxons(id) ON DELETE CASCADE
     )
     ''')
 
@@ -65,17 +64,17 @@ def load_default():
     cursor = taxonomy.cursor()
 
     taxas = [
-        (1, "Arthropoda", "Arthropod", "Insects, crustaceans and such", 0),
-        (2, "Insecta", "Insects", "Hexapod invertebrates", 1),
-        (5, "Hempitera", "True Bugs", "Bug", 2),
+        (12, "Arthropoda", "Arthropod", "Insects, crustaceans and such", 0),
+        (11, "Insecta", "Insects", "Hexapod invertebrates", 1),
+        (8, "Hempitera", "True Bugs", "Bug", 2),
         (6, "Heteroptera", "Typical Bugs", "Different wings!", 3),
-        (8, "Pyrrhocoridae", "Red Bugs", "They are red", 4),
-        (11, "Pyrrhocoris", "Cotton Stainers", "Yup, red", 5),
-        (12, "Pyrrhocoris apterus", "Firebug", "Red!", 6)
+        (5, "Pyrrhocoridae", "Red Bugs", "They are red", 4),
+        (2, "Pyrrhocoris", "Cotton Stainers", "Yup, red", 5),
+        (1, "Pyrrhocoris apterus", "Firebug", "Red!", 6)
     ]
 
     cursor.executemany('''
-    INSERT INTO taxonomic_rank (rank_id, name_latin, name_english, description, super_taxon_id) VALUES (?, ?, ?, ?, ?)
+    INSERT INTO taxons (rank_id, name_latin, name_english, description, super_taxon_id) VALUES (?, ?, ?, ?, ?)
     ''', taxas)
 
     taxonomy.commit()
@@ -87,8 +86,8 @@ def view_taxonomy():
     cursor = taxonomy.cursor()
 
     cursor.execute('''
-    SELECT taxonomic_rank.id, taxonomic_rank.name_latin, taxonomic_rank.description
-    FROM taxonomic_rank
+    SELECT taxons.id, taxons.name_latin, taxons.description
+    FROM taxons
     ''')
 
     rows = cursor.fetchall()
@@ -100,7 +99,7 @@ def view_taxonomy():
     taxonomy.close()
 
 
-def input_rank_id():
+def input_rank_id():    # Displays all taxonomic ranks
     taxonomy = sqlite3.connect('taxonomy.db')
     cursor = taxonomy.cursor()
 
@@ -112,10 +111,10 @@ def input_rank_id():
     rows = cursor.fetchall()
     while True:
         rank_id_list = []
+        print(f"{'Rank ID':<10} {'Name':<20}")
         for row in rows:
             rank_id_list.append(row[0])
-            print(f"Rank ID: {row[0]}, Name: {row[1]}")
-        print(rank_id_list)
+            print(f"{row[0]:<10} {row[1]:<20}")
         try:
             rank_id = int(input("Rank ID: "))
             if any(rank == rank_id for rank in rank_id_list):
@@ -133,20 +132,51 @@ def input_rank_id():
     return rank_id
 
 def input_supertaxon_id():
+    taxonomy = sqlite3.connect('taxonomy.db')
+    cursor = taxonomy.cursor()
+
+    cursor.execute('''
+        SELECT t.id, t.name_latin, r.name
+        FROM taxons t
+        JOIN ranks r ON t.rank_id = r.id
+        ''')
+    rows = cursor.fetchall()
+    while True:
+        supertaxon_id_list = []
+        print(f"{'Supertaxon ID':<20} {'Name':<30} {'Rank Name':<20}")
+        for row in rows:
+            supertaxon_id_list.append(row[0])
+            print(f"{row[0]:<20} {row[1]:<30} {row[2]:<20}")
+        try:
+            supertaxon_id = int(input("Supertaxon ID: "))
+            if any(supertaxon == supertaxon_id for supertaxon in supertaxon_id_list):
+                break
+            else:
+                print("------")
+                print("Supertaxon not found")
+                print("------")
+        except:
+            print("------")
+            print("ID must be an integer")
+            print("------")
+
+    taxonomy.close()
+    return supertaxon_id
 
 def add_taxonomy():
     taxonomy = sqlite3.connect('taxonomy.db')
     cursor = taxonomy.cursor()
     while True:
         new_rank_id = input_rank_id()
-        new_name_latin = input("Latin name")
-        new_name_english = input("English name")
-        new_description = input("Description")
-        new_supertaxon_id = input("Supertaxon ID")
+        new_supertaxon_id = input_supertaxon_id()
+
+        new_name_latin = input("Latin name ")
+        new_name_english = input("English name ")
+        new_description = input("Description ")
         break
     new_taxon = (new_rank_id, new_name_latin, new_name_english, new_description, new_supertaxon_id)
     cursor.execute('''
-    INSERT INTO taxonomic_rank (rank_id, name_latin, name_english, description, super_taxon_id) VALUES (?, ?, ?, ?, ?)
+    INSERT INTO taxons (rank_id, name_latin, name_english, description, super_taxon_id) VALUES (?, ?, ?, ?, ?)
     ''', new_taxon)
     taxonomy.commit()
     taxonomy.close()
